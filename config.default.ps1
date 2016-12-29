7ed8867b7bbf5aef5a6c2c5766272b9d6a07a818
Aleksandr Kokin (Akvelon) <v-alekok@microsoft.com>
2016-12-29 16:08:52 +0300

    Modified irregular hours in default config.

--- config.default.ps1
+++ config.default.ps1
@@ -1,6 +1,6 @@
 ﻿$api_token = ""
 
 $client_name = "Akvelon"
-$irregular_time_start = "23"
-$irregular_time_end = "7"
+$irregular_time_start = "21"
+$irregular_time_end = "8"
 $irregular_time_prefix = "Irregular hours - "
